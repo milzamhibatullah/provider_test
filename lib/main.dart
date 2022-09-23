9cd8e4c56abc4d57f2bf76479822b48553be7d3c
Milzam Hibatullah <hmilzam@gmail.com>
2022-09-24 06:48:18 +0700

    creating model and home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider_test/ui/home_screen.dart';
 
 void main() {
-
+  runApp(
+    MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (_) =>  HomeScreen(),
+      },
+    ),
+  );
 }
-
-
---
+++ lib/ui/home_screen.dart
@@ -0,0 +1,28 @@
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        iconTheme: const IconThemeData(color: Colors.black),
+        title: const Text('Provider Example Apps'),
+      ),
+      body: SafeArea(
+        child: Container(
+          height: MediaQuery
+              .of(context)
+              .size
+              .height,
+          width: MediaQuery
+              .of(context)
+              .size
+              .width,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
